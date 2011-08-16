875c9fee9f58b329d6d954ad5ce6a1302baf842a
Sunng <classicning@gmail.com>
2011-08-16 13:38:54 +0800

    add aleph dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clojalk "1.0.0-SNAPSHOT"
   :description "A beanstalkd clone in clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [aleph "0.2.0-beta1"]])
