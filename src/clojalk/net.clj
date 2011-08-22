7a944aa5ba7a62abb500816891e128b8123b512e
Sun Ning <classicning@gmail.com>
2011-08-22 21:58:50 +0800

    add network tier impl for put

--- src/clojalk/net.clj
+++ src/clojalk/net.clj
@@ -1,11 +1,12 @@
 (ns clojalk.net
   (:refer-clojure :exclude [use peek])
-  (:use [clojalk.core])
+  (:use [clojalk core utils])
   (:use [clojalk.net.protocol])
   (:use [aleph.tcp])
   (:use [lamina.core])
   (:use [gloss.core]))
 
+;; this is for test and debug only
 (defn echo-handler [ch client-info]
   (receive-all ch
     #(if-let [msg %]
@@ -18,21 +19,47 @@
          
            (enqueue ch "UNKNOWN_COMMAND"))))))
 
-;; all sessions
+;; sessions 
 (defonce sessions (ref {}))
 
-;; 
-(defn handle-command [ch cmd args])
+(defn get-or-create-session [remote-addr type]
+  (dosync
+    (if-not (contains? @sessions remote-addr)
+      (alter sessions assoc remote-addr (open-session type))))
+  (@sessions remote-addr))
 
-(defn command-dispatcher [ch client-info]
+(defn close-session [remote-addr]
+  (dosync
+    (alter sessions dissoc remote-addr)))
+
+;; server handlers
+(defn on-put [ch session args]
+  (try
+    (let [priority (as-int (first args))
+          delay (as-int (second args))
+          ttr (as-int (nth args 2))
+          body (last args)
+          job (put session priority delay ttr body)]
+      (if job
+        (enqueue ch (str "INSERTED " (:id job)))))
+    (catch NumberFormatException e (enqueue ch "BAD_FORMAT"))))
+
+(defn command-dispatcher [ch client-info cmd args]
+  (let [remote-addr (:remote-addr client-info)]
+    (case cmd
+      "put" (on-put ch (get-or-create-session remote-addr :producer) args))))
+
+(defn default-handler [ch client-info]
   (receive-all ch
     #(if-let [msg %]
        (if (seq? msg)
-         (handle-command ch (first msg) (rest msg))
+         (try 
+           (command-dispatcher ch client-info (first msg) (rest msg))
+           (catch Exception e (enqueue ch "INTERNAL_ERROR")))
          (enqueue ch "UNKNOWN_COMMAND")))))
 
 (defn start-server [port]
-  (start-tcp-server echo-handler {:port port, :frame beanstalkd-codec}))
+  (start-tcp-server default-handler {:port port, :frame beanstalkd-codec}))
 
 (defn -main []
   (do
