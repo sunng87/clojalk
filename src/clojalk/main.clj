e221daf39b0a81b01bc2ce997ebcf9336c8e2f55
Sun Ning <classicning@gmail.com>
2011-08-26 23:42:14 +0800

    add configuration file

--- src/clojalk/main.clj
+++ src/clojalk/main.clj
@@ -1,10 +1,16 @@
 (ns clojalk.main
   (:gen-class)
   (:refer-clojure :exclude [use peek])  
-  (:use [clojalk net core utils]))
+  (:use [clojalk net core utils])
+  (:use [clojure.contrib.properties]))
 
-(defn -main []
-  (do
+(defn property [properties key]
+  (.getProperty properties key))
+
+(defn -main [& args]
+  (let [prop-file-name (or (first args) "clojalk.properties")
+        props (read-properties prop-file-name)]
     (start-tasks)
-    (start-server 10000)
-    (println "Clojalk server started")))
+    (start-server (as-int (property props "server.port")))
+    (println (str "Clojalk server started, listening on " 
+                  (property props "server.port")))))
