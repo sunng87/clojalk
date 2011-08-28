cf8d2422a34d6d0ca2a930e6b43a2784f0016324
Sun Ning <classicning@gmail.com>
2011-08-28 16:03:22 +0800

    add benchmark scripts

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [aleph "0.2.0-beta1"]]
+  :dev-dependencies [[org.clojars.sunng/beanstalk "1.0.5"]
+                     [lein-exec "0.1"]]
   :main clojalk.main)
---
+++ scripts/bench.clj
@@ -0,0 +1,70 @@
+(ns bench
+  (:refer-clojure :exclude [use peek read])
+  (:use [beanstalk.core]))
+
+(defn sleep [timemillis]
+  (Thread/sleep timemillis))
+
+(defn byte-length [s]
+  (alength (.getBytes s "utf8")))
+
+(defn make-conn []
+  (new-beanstalk 11300))
+
+(def task-body
+  "<request><name>Activation</name><email>sunng@sunng.info</email></request>")
+
+(defn do-put [conn]
+  (put conn 
+       (rand-int 5000)  ; priority
+       (rand-int 5) ; delay
+       0 
+       (byte-length task-body) 
+       task-body))
+
+(defn do-reserve [conn]
+  (:id (reserve conn)))
+
+(defn do-delete [conn id]
+  (delete conn id))
+
+(def *tube-name* "bench-tube")
+
+(defn producer []
+  (println "starting producer")
+  (let [conn (make-conn)]
+    (use conn *tube-name*)
+    (loop []
+      (do-put conn)
+      (sleep (rand-int 30))
+      (recur))))
+
+(defn worker []
+  (println "starting worker")
+  (let [conn (make-conn)]
+    (watch conn *tube-name*)
+    (loop []
+      (let [id (do-reserve conn)]
+        (sleep (rand-int 100))
+        (do-delete conn id)
+        (recur)))))
+
+(defn monitor []
+  (println "starting monitor")
+  (let [conn (make-conn)]
+    (use conn *tube-name*)
+    (loop []
+      (println (:stats (stats-tube conn *tube-name*)))
+      (sleep 5000)
+      (recur))))
+
+(defn run-in-thread [runnable]
+  (let [t (Thread. runnable)]
+    (.setDaemon t false)
+    (.start t)))
+
+(doall (map run-in-thread (take 5 (repeat producer))))
+(doall (map run-in-thread (take 10 (repeat worker))))
+(run-in-thread monitor)
+(println "benchmark started")
+
