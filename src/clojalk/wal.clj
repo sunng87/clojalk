eca205ca563bb4dbe41569846212f403a734d326
Sun Ning <classicning@gmail.com>
2011-09-13 22:03:51 +0800

    more work on wal

--- src/clojalk/wal.clj
+++ src/clojalk/wal.clj
@@ -1,12 +1,13 @@
 (ns clojalk.wal
-  (:import [java.nio ByteBuffer]))
+  (:use clojure.java.io)
+  (:import [java.nio ByteBuffer IntBuffer]))
 
 (def job-base-size 58)
 
 (defn- as-bytes [s]
   (.getBytes s "UTF8"))
 
-(defn- state-enum [state]
+(defn state-enum [state]
   (short (case state
     :ready 0
     :delayed 1
@@ -15,7 +16,7 @@
     :invalid 4
     -1)))
 
-(defn- enum-state [e]
+(defn enum-state [e]
   (nth [:ready :delayed :reserved :buried :invalid] e))
 
 ;;
@@ -73,5 +74,63 @@
         (.putInt buffer 0)))
     buffer))
 
+;; read a fixed size of bytes from stream
+(defn- read-bytes [stream size]
+  (let [bytes (byte-array size)]
+    (do
+      (.read stream bytes)
+      bytes)))
 
+;; Read a job entry from stream
+;; To test if a job entry is a full entry, test if its :tube is not nil
+;; 
+;; I use a transient map here to simplify the code and improve performance
+(defn read-job [stream]
+  (let [base-bytes (ByteBuffer/wrap (read-bytes stream job-base-size))
+        tube-name-length (.get (IntBuffer/wrap (read-bytes stream 4)))
+        tube-name (if (zero? tube-name-length) 
+                    nil 
+                    (String. (read-bytes stream tube-name-length) "UTF8"))
+        job-body-length (.get (IntBuffer/wrap (read-bytes stream 4)))
+        job-body (if (zero? job-body-length) 
+                   nil 
+                   (String. (read-bytes stream tube-name-length) "UTF8"))
+        
+        job (transient {})]
+    (assoc! job :id (.getLong base-bytes))
+    (assoc! job :delay (.getInt base-bytes))
+    (assoc! job :ttr (.getInt base-bytes))
+    (assoc! job :priority (.getInt base-bytes))
+    (assoc! job :created_at (.getLong base-bytes))
+    (assoc! job :deadline_at (.getLong base-bytes))
+    (assoc! job :state (enum-state (.getShort base-bytes)))
+    (assoc! job :reserves (.getInt base-bytes))
+    (assoc! job :timeouts (.getInt base-bytes))
+    (assoc! job :releases (.getInt base-bytes))
+    (assoc! job :buries (.getInt base-bytes))
+    (assoc! job :kicks (.getInt base-bytes))
+    (if-not (nil? tube-name) (assoc! job :tube tube-name))
+    (if-not (nil? job-body) (assoc! job :body job-body))
+    (persistent! job)))
 
+;; Read a bin file into a vector of job entries
+(defn read-file [bin-log-file]
+  (with-open [stream (input-stream bin-log-file)]
+    (loop [s stream jobs []]
+      (if (zero? (.available s))
+        jobs
+        (recur s (conj jobs (read-job s)))))))
+
+;; Scan directory to find files whose name endswith .bin
+(defn scan-dir [dir-path]
+  (filter #(.endsWith (.getName %) ".bin") (.listFiles (file dir-path))))
+
+;; default clojalk log directory
+(def *clojalk-log-dir*)
+
+;; Replay logs and load jobs
+;; 
+(defn replay-logs []
+  (let [bin-log-files (scan-dir *clojalk-log-dir*)]
+    (doseq [bin-log-file bin-log-files]
+      (read-file bin-log-file)))) ;;TODO
