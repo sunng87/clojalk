5248d4f6cc614c9a4c3fd9220bad1e385f242678
Sunng <classicning@gmail.com>
2011-09-20 13:47:12 +0800

    Fix error in test cases after adding type hints

--- test/clojalk/test/net.clj
+++ test/clojalk/test/net.clj
@@ -2,7 +2,8 @@
   (:refer-clojure :exclude [use peek])
   (:use [clojalk core net utils])
   (:use [lamina.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:import java.net.InetSocketAddress))
 
 (defmacro with-ch [ch data-in addr-in & body]
   `(let [~ch (channel ~data-in)
@@ -14,22 +15,22 @@
   `(receive-all ~ch #(is (~test %))))
 
 (deftest test-put
-  (with-ch ch ["PUT" "5" "0" "9" "abc"] "127.0.0.1:19875"
+  (with-ch ch ["PUT" "5" "0" "9" "abc"] (InetSocketAddress. "127.0.0.1" 19875) 
     (is-ch ch #(= "INSERTED" (first %)))))
 
 (deftest test-use
-  (with-ch ch ["USE" "tomcat"] "127.0.0.1:19876"
+  (with-ch ch ["USE" "tomcat"] (InetSocketAddress. "127.0.0.1" 19876)
     (is-ch ch #(= ["USING" "tomcat"] %))))
 
 (deftest test-watch
-  (with-ch ch ["WATCH" "tomcat"] "127.0.0.1:19877"
+  (with-ch ch ["WATCH" "tomcat"] (InetSocketAddress. "127.0.0.1" 19877)
     (is-ch ch #(= ["WATCHING" "2"] %))))
 
 (deftest test-ignore
-  (with-ch ch ["IGNORE" "default"] "127.0.0.1:19878"
+  (with-ch ch ["IGNORE" "default"] (InetSocketAddress. "127.0.0.1" 19878)
     (is-ch ch #(= ["NOT_IGNORED"] %))))       
 
 (deftest test-peekdelayed
-  (with-ch ch ["PEEK-DELAYED"] "127.0.0.1:19879"
+  (with-ch ch ["PEEK-DELAYED"] (InetSocketAddress. "127.0.0.1" 19879) 
     (is-ch ch #(= ["NOT_FOUND"] %)))) 
 
