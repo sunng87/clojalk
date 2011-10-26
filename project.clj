30086d7ef39055cd2ac207ae5d55ad1cb6538258
Sun Ning <classicning@gmail.com>
2011-10-26 23:37:59 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A beanstalkd clone in clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [aleph "0.2.0-beta2"]]
+                 [aleph "0.2.0-rc1"]]
   :dev-dependencies [[org.clojars.sunng/beanstalk "1.0.5"]
                      [lein-exec "0.1"]
                      [lein-marginalia "0.6.0"]]
