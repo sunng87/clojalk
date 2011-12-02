5b5412a913d60e8268db60be72c60a7c9915e416
Sunng <classicning@gmail.com>
2011-12-02 15:37:07 +0800

    upgrade to clojure 1.3.0

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject clojalk "1.0.0-SNAPSHOT"
   :description "A beanstalkd clone in clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [aleph "0.2.0"]]
-  :dev-dependencies [[org.clojars.sunng/beanstalk "1.0.5"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                    [aleph "0.2.1-SNAPSHOT"]]
+  :dev-dependencies [[org.clojars.sunng/beanstalk "1.0.6"]
                      [lein-exec "0.1"]
-                     [lein-marginalia "0.6.0"]]
+                     [lein-marginalia "0.6.1"]]
   :warn-on-reflection true
   :main clojalk.main)
